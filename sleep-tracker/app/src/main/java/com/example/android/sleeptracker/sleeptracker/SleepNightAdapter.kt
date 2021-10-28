25018aa8e814b04a10ba9f9e3fc595d6d9a6deb6
Andrew Weiss <andreivayss@gmail.com>
2021-10-28 11:48:53 +0700

    Imlemented List adapter and DiffUtil

--- sleep-tracker/app/src/main/java/com/example/android/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ sleep-tracker/app/src/main/java/com/example/android/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -1,33 +1,42 @@
 package com.example.android.sleeptracker.sleeptracker
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.sleeptracker.R
 import com.example.android.sleeptracker.convertDurationToFormatted
 import com.example.android.sleeptracker.convertNumericQualityToString
 import com.example.android.sleeptracker.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
-    var data = listOf<SleepNight>()
-        @SuppressLint("NotifyDataSetChanged")
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+/**
+ * An adapter that provides a list of [SleepNight] to a RecyclerView
+ */
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
-    override fun getItemCount() = data.size
-
+    /**
+     * Part of RecyclerView adapter, called when RecyclerView needs to show an item
+     *
+     * The ViewHolder passed may be recycled, so make sure that this sets any properties that
+     * may have been set previously
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
+
+    /**
+     * Part of RecyclerView adapter, called when RecyclerView need a new [ViewHolder],
+     *
+     * A ViewHolder holds a view for the [RecyclerView] as well as providing additional information
+     * to the RecyclerView such as when on the screen it was last drawn during scrolling.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
@@ -40,17 +49,20 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         private val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
 
         fun bind(item: SleepNight) {
-            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            sleepLength.text =
+                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
             quality.text = convertNumericQualityToString(item.sleepQuality, res)
-            qualityImage.setImageResource(when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
+            qualityImage.setImageResource(
+                when (item.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                }
+            )
         }
 
         companion object {
@@ -63,5 +75,21 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
             }
         }
 
+    }
+
+    /**
+     * Callback for calculating the diff between two non-null items in a list.
+     *
+     * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+     * list that's been passed to `submitList`.
+     */
+    class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId == newItem.nightId
+        }
+
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem == newItem
+        }
     }
 }
